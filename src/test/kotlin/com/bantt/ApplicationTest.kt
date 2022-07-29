ce6312cd898496fd92c452b7491a193e58c28a95
= <=>
2022-07-29 16:19:38 +0300

    import optimization

--- src/test/kotlin/com/bantt/ApplicationTest.kt
+++ src/test/kotlin/com/bantt/ApplicationTest.kt
@@ -1,30 +1,12 @@
 package com.bantt
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.locations.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.server.sessions.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.hsts.*
-import io.ktor.server.plugins.httpsredirect.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.websocket.*
-import io.ktor.websocket.*
-import java.time.Duration
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import com.bantt.plugins.configureRouting
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.bantt.plugins.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
