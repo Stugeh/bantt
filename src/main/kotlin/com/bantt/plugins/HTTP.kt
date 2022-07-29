559a16ef0b1a9bbb0f0e51481dc41f1b858e74cb
= <=>
2022-07-29 16:18:15 +0300

    disabled https

--- src/main/kotlin/com/bantt/plugins/HTTP.kt
+++ src/main/kotlin/com/bantt/plugins/HTTP.kt
@@ -1,13 +1,9 @@
 package com.bantt.plugins
 
 import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.hsts.*
-import io.ktor.server.plugins.httpsredirect.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureHTTP() {
     install(CORS) {
@@ -22,14 +18,14 @@ fun Application.configureHTTP() {
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
-    install(HSTS) {
-        includeSubDomains = true
-    }
-    install(HttpsRedirect) {
-        // The port to redirect to. By default 443, the default HTTPS port.
-        sslPort = 443
-        // 301 Moved Permanently, or 302 Found redirect.
-        permanentRedirect = true
-    }
+//    install(HSTS) {
+//        includeSubDomains = true
+//    }
+//    install(HttpsRedirect) {
+//        // The port to redirect to. By default 443, the default HTTPS port.
+//        sslPort = 443
+//        // 301 Moved Permanently, or 302 Found redirect.
+//        permanentRedirect = true
+//    }
 
 }
