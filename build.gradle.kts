e23a5e650cfc23c3411963491a9809eed0077293
= <=>
2022-10-10 17:58:38 +0300

    white space

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ group = "com.bantt"
 version = "0.0.1"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
