352f1783f3298c9f887aabd9fa0aff091ee1e547
= <=>
2022-10-20 09:13:58 +0300

    added handlers to turn db row to User

--- src/main/kotlin/com/bantt/models/Server.kt
+++ src/main/kotlin/com/bantt/models/Server.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.util.*
 
@@ -17,6 +18,8 @@ class Server(id: EntityID<UUID>) : UUIDEntity(id) {
     val updatedAt by Servers.updatedAt
 
     val owner by User referencedOn Servers.owner
+    fun toResult() =
+        ServerResult(id.toString(), name, private, createdAt.toString(), updatedAt.toString(), owner.id.toString())
 }
 
 object Servers : UUIDTable() {
@@ -28,7 +31,27 @@ object Servers : UUIDTable() {
     val owner = reference("owner", Users)
 }
 
+data class ServerResult(
+    val id: String,
+    val name: String,
+    val private: Boolean,
+    val createdAt: String,
+    val updatedAt: String,
+    val owner: String
+)
+
 data class NewServer(
     val name: String,
     val private: Boolean
 )
+
+fun rowToServer(row: ResultRow): ServerResult {
+    return ServerResult(
+        row[Servers.id].toString(),
+        row[Servers.name],
+        row[Servers.private],
+        row[Servers.createdAt].toString(),
+        row[Servers.updatedAt].toString(),
+        row[Servers.owner].toString()
+    )
+}
