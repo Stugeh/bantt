7fefa6844f5907c4cc7a9243fa54d6c7b202e77a
= <=>
2022-12-18 17:46:46 +0200

    added subscriber list to servers

--- src/main/kotlin/com/bantt/models/Server.kt
+++ src/main/kotlin/com/bantt/models/Server.kt
@@ -3,8 +3,9 @@ package com.bantt.models
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
-data class Channel(
+data class Server(
     val name: String,
     val messages: List<Message> = listOf(),
+    val subscribers: List<ObjectId> = listOf(),
     @BsonId val _id: ObjectId = ObjectId()
 )
